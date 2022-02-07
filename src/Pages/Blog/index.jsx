34012a446eaa6e269954e54f6c2b181115b5bc59
Gagan Shri <gagan.nema@audacioustechnologies.com>
2022-02-07 17:58:52 +0530

    some design changes

--- src/Pages/Blog/index.jsx
+++ src/Pages/Blog/index.jsx
@@ -1,18 +1,19 @@
 import React from "react";
 // import styles from "./styles.module.css"
 import Navbar from "../../Component/Navbar";
-const About = () =>{
-    return(
+
+const Blog = () => {
+    return (
         <div>
-        <Navbar/>
-           <div>
-              <h2>About Page</h2>
-              <hr />
+            <Navbar />
+            <div>
+                <h2>Blog Page</h2>
+                <hr />
                 <p>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                 </p>
             </div>
-       </div>
+        </div>
     )
 }
-export default About;
+export default Blog;
--- src/Routing/index.jsx
+++ src/Routing/index.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import {Routes, Route} from "react-router-dom";
 import Home from "../Pages/Home";
 import About from "../Pages/About";
+import Blog from "../Pages/Blog";
+import Contact from "../Pages/Contact";
 const Routing = () => {
     return (
         <div>
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
+                <Route path="/blog" element={<Blog />} />
+                <Route path="/contact" element={<Contact />} />
             </Routes>
         </div>
     )
